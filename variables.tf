c0bcbeefa66ce09b918791b6aae0b4d203e600ab
Milind Kohale <milindkohale87@gmail.com>
2022-11-27 16:25:29 +0530

    commiting variables file

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "service_name" {
+  type    = string
+  default = "assignment"
+}
+
+variable "instance_root_disc" {
+  default = "30"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
